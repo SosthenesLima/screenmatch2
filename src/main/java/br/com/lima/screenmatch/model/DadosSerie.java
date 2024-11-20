60c13679515e5691f7d8788f48a88d8c8487a780
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-11-19 22:47:57 -0300

    Desserializando dados commit

--- src/main/java/br/com/lima/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/lima/screenmatch/model/DadosSerie.java
@@ -1,4 +1,9 @@
 package br.com.lima.screenmatch.model;
 
-public record DadosSerie() {
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTempadas,
+                         @JsonAlias("imdbRating") String avaliacao) {
 }
