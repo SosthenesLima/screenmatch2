809888a2eef3b0adc3a52701fe8834ea20eeedc7
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-11-23 10:58:49 -0300

    Criando o menu de interação com o usuário02 commit

--- src/main/java/br/com/lima/screenmatch/principal/Principal.java
+++ src/main/java/br/com/lima/screenmatch/principal/Principal.java
@@ -1,13 +1,22 @@
 package br.com.lima.screenmatch.principal;
 
+import br.com.lima.screenmatch.service.ConsumoApi;
+
 import java.util.Scanner;
 
 public class Principal {
 
     private Scanner leitura = new Scanner(System.in);
+
+    private final String ENDERECO =  "https://omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=6585022c";
+
     public void exibeMenu(){
-        System.out.println("Digite o nome da série para ");
+        System.out.println("Digite o nome da série para buscar: ");
         var nomeSerie = leitura.nextLine();
+        var consumoApi = new ConsumoApi();
+        var json = consumoApi.obterDados(ENDERECO + nomeSerie.replace("", "+") + API_KEY);
+
         //https://omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6585022c
 
     }
