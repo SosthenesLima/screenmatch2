910ded9fa3631f627e68ff47303039590609008f
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-11-23 18:15:31 -0300

    Trabalhando na coleção de dados commit

--- src/main/java/br/com/lima/screenmatch/principal/Principal.java
+++ src/main/java/br/com/lima/screenmatch/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.lima.screenmatch.principal;
 
+import br.com.lima.screenmatch.model.DadosEpisodio;
 import br.com.lima.screenmatch.model.DadosSerie;
 import br.com.lima.screenmatch.model.DadosTemporada;
 import br.com.lima.screenmatch.service.ConsumoApi;
@@ -30,11 +31,18 @@ public class Principal {
         List<DadosTemporada> tempradas = new ArrayList<>();
 
 		for ( int i = 1; i<= dados.totalTemporadas(); i++){
-			json = consumo.obterDados("https://omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6585022c");
+			json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +"&season=" + i + API_KEY);
 			DadosTemporada dadosTemprada = conversor.obterDados(json, DadosTemporada.class);
 			tempradas.add(dadosTemprada);
 	}
 		tempradas.forEach(System.out::println);
+
+        for(int  i = 0; i < dados.totalTemporadas(); i++){
+            List<DadosEpisodio> episodiosTemporada = tempradas.get(i).episodios();
+            for(int j = 0; j < episodiosTemporada.size(); j++){
+
+            }
+        }
 
     }
 }
