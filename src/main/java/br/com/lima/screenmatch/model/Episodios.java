e7168099cdeedcc98b9e121de49a4bfdc73ee6ec
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-11-26 23:16:06 -0300

    Uma nova classe para lidar com os dados do episódio01 commit

--- src/main/java/br/com/lima/screenmatch/model/Episodios.java
+++ src/main/java/br/com/lima/screenmatch/model/Episodios.java
@@ -1,4 +1,16 @@
 package br.com.lima.screenmatch.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+import java.time.LocalDate;
+
 public class Episodios {
+    private Integer temporada;
+    private String titulo,
+
+    private Integer numeroEpisodio;
+
+   private Double avaliacao;
+    private LocalDate dataLancamento;
 }
+
