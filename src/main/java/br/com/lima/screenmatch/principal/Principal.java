3cc4998e0e0ed25f87de30141b3f3dfcc50420c2
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-11-30 17:53:17 -0300

    Exibindo as etapas de uma stream02 commit

--- src/main/java/br/com/lima/screenmatch/principal/Principal.java
+++ src/main/java/br/com/lima/screenmatch/principal/Principal.java
@@ -71,11 +71,13 @@ public class Principal {
         System.out.println("\n Top 10 episódios");
         dadosEpisodios.stream()
                         .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
-                        .peek(e -> System.out.println( "Primeiro Filtro(N/A)" + e))
+                        .peek(e -> System.out.println( "Primeiro Filtro(N/A) " + e))
                         .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
-                        .peek(e -> System.out.println( "Ordenação" + e))
+                        .peek(e -> System.out.println( "Ordenação " + e))
                         .limit(10)
+                        .peek(e -> System.out.println( "Limite " + e))
                         .map(e-> e.titulo().toUpperCase())
+                        .peek(e -> System.out.println( "Mapeamento " + e))
                         .forEach(System.out::println);
 
         /* List<Episodios> episodios = tempradas.stream()
