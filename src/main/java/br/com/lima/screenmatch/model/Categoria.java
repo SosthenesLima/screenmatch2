9363a8e45616237a2533dd7f79ccf24844321619
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-12-15 09:38:52 -0300

    Modelando categorias e séries01 commit

--- src/main/java/br/com/lima/screenmatch/model/Categoria.java
+++ src/main/java/br/com/lima/screenmatch/model/Categoria.java
@@ -1,5 +1,16 @@
 package br.com.lima.screenmatch.model;
 
 public enum Categoria {
+    ACAO("Action"),
+    ROMANCE("Romance"),
+    COMEDIA("Comedy"),
+    DRAMA("Drama"),
+    CRIME("Crime"),
+    TERROR("Terror");
 
+    private String categoriaOmdb;
+
+    Categoria(String categoriaOmdb) {
+        this.categoriaOmdb = categoriaOmdb;
+    }
 }
--- src/main/java/br/com/lima/screenmatch/model/Serie.java
+++ src/main/java/br/com/lima/screenmatch/model/Serie.java
@@ -3,6 +3,9 @@ package br.com.lima.screenmatch.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import jdk.jfr.Category;
 
+import java.util.Optional;
+import java.util.OptionalDouble;
+
 public class Serie {
 
     private String titulo;
@@ -12,5 +15,11 @@ public class Serie {
     private String atores;
     private String poster;
     private String sinopse;
+
+    public Serie(DadosSerie dadosSerie) {
+        this.titulo = dadosSerie.titulo();
+        this.totalTemporadas = dadosSerie.totalTemporadas();
+        this.avaliacao = OptionalDouble.of(Double.valueOf(dadosSerie.avaliacao())).orElse(0.0);
+    }
 
 }
