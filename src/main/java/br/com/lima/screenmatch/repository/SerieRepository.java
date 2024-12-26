954e115cc00598fabb4b3b8812e99735a181f2b9
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-12-26 11:45:35 -0300

    Salvando dados no banco02 commit

--- src/main/java/br/com/lima/screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/lima/screenmatch/repository/SerieRepository.java
@@ -1,4 +1,7 @@
 package br.com.lima.screenmatch.repository;
 
-public interface SerieRepository {
+import br.com.lima.screenmatch.model.Serie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SerieRepository extends JpaRepository<Serie, Long> {
 }
