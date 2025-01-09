8d2b72b5e64f1977650966c7176f5bcdc350bdbd
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2025-01-09 11:55:15 -0300

    commit Utilizando o Spring Web01

--- src/main/java/br/com/lima/screenmatch/ScreenmatchApplicationSemWeb.java
+++ src/main/java/br/com/lima/screenmatch/ScreenmatchApplicationSemWeb.java
@@ -1,3 +1,4 @@
+/*
 package br.com.lima.screenmatch;
 import br.com.lima.screenmatch.principal.Principal;
 import br.com.lima.screenmatch.repository.SerieRepository;
@@ -7,15 +8,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-
 @SpringBootApplication
-public class ScreenmatchApplication  {
+public class ScreenmatchApplicationSemWeb implements CommandLineRunner {
 
+	@Autowired
+	private SerieRepository repositorio;
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(ScreenmatchApplication.class, args);
+		SpringApplication.run(ScreenmatchApplicationSemWeb.class, args);
 	}
 
-
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(repositorio);
+		principal.exibeMenu();
+	}
 }
+
+*/
