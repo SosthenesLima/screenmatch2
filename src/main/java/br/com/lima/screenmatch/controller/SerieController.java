5e07d65a55d0de81b231a8b94ddf53a19d5419ec
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2025-01-10 20:37:00 -0300

    commit Exibindo uma mensagem no navegador03

--- src/main/java/br/com/lima/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/lima/screenmatch/controller/SerieController.java
@@ -1,6 +1,8 @@
 package br.com.lima.screenmatch.controller;
 
 import br.com.lima.screenmatch.model.Serie;
+import br.com.lima.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,11 @@ import java.util.List;
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository repositorio;
+
     @GetMapping("/series")
     public List<Serie> obterSeries() {
-        return
+        return repositorio.findAll();
     }
 }
