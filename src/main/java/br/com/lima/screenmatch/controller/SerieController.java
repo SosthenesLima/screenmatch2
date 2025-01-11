0179cdbfb79e71b7c9251b9ee8aa42ec76c375d7
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2025-01-11 19:53:20 -0300

    commit Criando um serviço para as séries01

--- src/main/java/br/com/lima/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/lima/screenmatch/controller/SerieController.java
@@ -1,8 +1,6 @@
 package br.com.lima.screenmatch.controller;
-
 import br.com.lima.screenmatch.dto.SerieDTO;
-import br.com.lima.screenmatch.model.Serie;
-import br.com.lima.screenmatch.repository.SerieRepository;
+import br.com.lima.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,18 +12,15 @@ import java.util.stream.Collectors;
 public class SerieController {
 
     @Autowired
-    private SerieRepository repositorio;
+    private SerieService servico;
 
     @GetMapping("/series")
     public List<SerieDTO> obterSeries() {
-        return repositorio.findAll()
-                .stream()
-                .map(s ->new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-                .collect(Collectors.toList());
-    }
+        return servico.obterTodasAsSeries();
 
-   // @GetMapping("/inicio")
-   // public String retornarInicio() {
+        // @GetMapping("/inicio")
+        // public String retornarInicio() {
         //return "Bem Vindo ao ScreeMatch - 2025";
-   // }
+        // }
+    }
 }
--- src/main/java/br/com/lima/screenmatch/service/SerieService.java
+++ src/main/java/br/com/lima/screenmatch/service/SerieService.java
@@ -1,4 +1,24 @@
 package br.com.lima.screenmatch.service;
 
+import br.com.lima.screenmatch.dto.SerieDTO;
+import br.com.lima.screenmatch.model.Serie;
+import br.com.lima.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class SerieService {
+
+    @Autowired
+    private SerieRepository repositorio;
+
+    public List<SerieDTO> obterTodasAsSeries() {
+        return repositorio.findAll()
+                .stream()
+                .map(s ->new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+    }
 }
