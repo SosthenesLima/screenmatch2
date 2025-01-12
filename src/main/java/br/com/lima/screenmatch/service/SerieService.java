b4166b52c2065fa028d0ed8544d1f3e714a3876f
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2025-01-12 11:05:22 -0300

    commit Exibindo as top séries do banco01

--- src/main/java/br/com/lima/screenmatch/service/SerieService.java
+++ src/main/java/br/com/lima/screenmatch/service/SerieService.java
@@ -23,8 +23,12 @@ public class SerieService {
     }
 
     public List<SerieDTO> obterTop5Series() {
-        return repositorio.findTop5ByOrderByAvaliacaoDesc()
-                .stream()
+        return converteDados(repositorio.findTop5ByOrderByAvaliacaoDesc());
+
+    }
+
+    private List<SerieDTO> converteDados(List<Serie> series) {
+        return series.stream()
                 .map(s ->new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
                 .collect(Collectors.toList());
 
