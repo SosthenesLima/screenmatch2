b9c873e1ff2610fa0fcb4940b757d4e8c15d7642
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2025-01-13 09:58:34 -0300

    commit Retornando os detalhes da série

--- src/main/java/br/com/lima/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/lima/screenmatch/controller/SerieController.java
@@ -3,6 +3,7 @@ import br.com.lima.screenmatch.dto.SerieDTO;
 import br.com.lima.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,5 +38,10 @@ public class SerieController {
     public List<SerieDTO> obterLancamentos() {
         return servico.obterLancamentos();
 
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obterPorId(@PathVariable Long id) {
+        return servico.obterPorId(id);
     }
 }
